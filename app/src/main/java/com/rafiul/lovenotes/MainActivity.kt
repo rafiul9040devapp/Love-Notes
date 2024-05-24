45cdd17dba0f9451d032e88334167c9ba0c670f6
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-05-24 10:03:51 +0600

    problem of appbar is solved

--- app/src/main/java/com/rafiul/lovenotes/MainActivity.kt
+++ app/src/main/java/com/rafiul/lovenotes/MainActivity.kt
@@ -17,12 +17,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
         setUpViewModel()
-
-
     }
 
 
