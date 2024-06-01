bc62e33b047b17d497cf6050c4d685cac38e5782
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-06-01 12:40:01 +0600

    coroutine scope is done

--- app/src/main/java/com/rafiul/lovenotes/repository/NoteRepositoryImp.kt
+++ app/src/main/java/com/rafiul/lovenotes/repository/NoteRepositoryImp.kt
@@ -4,14 +4,28 @@ package com.rafiul.lovenotes.repository
 import androidx.lifecycle.LiveData
 import com.rafiul.lovenotes.database.NoteDao
 import com.rafiul.lovenotes.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class NoteRepositoryImp @Inject constructor(private val noteDao: NoteDao) : NoteRepository {
-    override suspend fun insertNote(note: Note) = noteDao.insertNote(note)
+    override suspend fun insertNote(note: Note) {
+        withContext(Dispatchers.IO) {
+            noteDao.insertNote(note)
+        }
+    }
 
-    override suspend fun updateNote(note: Note) = noteDao.updateNote(note)
+    override suspend fun updateNote(note: Note) {
+        withContext(Dispatchers.IO) {
+            noteDao.updateNote(note)
+        }
+    }
 
-    override suspend fun deleteNote(note: Note) = noteDao.deleteNote(note)
+    override suspend fun deleteNote(note: Note) {
+        withContext(Dispatchers.IO) {
+            noteDao.deleteNote(note)
+        }
+    }
 
     override fun getAllNotes(): LiveData<List<Note>> = noteDao.getAllNotes()
 
